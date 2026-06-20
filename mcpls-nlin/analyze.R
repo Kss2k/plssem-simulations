d9b916abf5c844214156cca938f88f715a219484
kss2k <slupphaugkjell@gmail.com>
2026-06-20 14:36:06 +0200

    Work on plots

---
+++ mcpls-nlin/analyze.R
@@ -0,0 +1,158 @@
+devtools::load_all()
+set_project_root()
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(patchwork)
+library(scales)
+
+rdir <- "mcpls-nlin/results/"
+files <- dir(rdir)
+paths <- paste0(rdir, files)
+ids   <- abbreviate(files, 6)
+
+read <- function(path) {
+  name <- last(stringr::str_split_1(path, "/"))
+  df <- read.csv(path)
+  id <- ids[[name]]
+  df$id <- paste0(id, "-", df$id)
+  df
+}
+
+
+getModelLoading <- function(par, true) {
+  idx <- which.max(par == "X=~x1")
+  true[[idx]]
+}
+
+methods_ordered <- c("PLS", "PLSc", "MC-OrdPLSc", "Mplus")
+
+df <- do.call(rbind, lapply(paths, read)) |>
+  mutate(
+    bias = est - true,
+    method = factor(method,
+      levels = methods_ordered,
+      labels = methods_ordered
+    )
+  ) |>
+  group_by(id, method) |>
+  mutate(
+    parcombo = paste0(paste0(par, "=", true), collapse = ","),
+    loadings = getModelLoading(par, true)
+  )
+
+# We try to split the simulations into sample size, and model parameter combos
+# Within each we look at the performance
+simsplit <- expand.grid(
+  n = sort(unique(df$n)),
+  loadings = sort(unique(df$loadings))
+)
+
+par2tex <- list(
+  `Y~X`   = "gamma[1]",
+  `Y~Z`   = "gamma[2]",
+  `Y~X:Z` = "gamma[3]",
+  `Y=~y1` = "lambda[7]",
+  `Y=~y2` = "lambda[8]",
+  `Y=~y3` = "lambda[9]"
+)
+
+# Count inadmissibles
+admissible <- group_by(df, id, method, loadings, ncat, skew, n) |> 
+  summarize(admissible = unique(admissible)) |>
+  group_by(method, loadings, ncat, skew, n) |> 
+  summarize(nruns = length(admissible),
+            ninadmissible = sum(!admissible),
+            pinadmissible = sum(!admissible)/length(admissible))
+
+print(admissible, n = 500)
+
+
+plots_inadmissible <- vector("list", NROW(simsplit))
+plots_bias_b3 <- vector("list", NROW(simsplit))
+
+for (i in seq_len(NROW(simsplit))) {
+  # ----------------------------------------------------------------------------
+  # Simulation settings
+  # ----------------------------------------------------------------------------
+
+  n.i <- simsplit$n[[i]]
+  loadings.i <- simsplit$loadings[[i]]
+
+  # ----------------------------------------------------------------------------
+  # Inadmissible Solutions
+  # ----------------------------------------------------------------------------
+
+  dodge <- position_dodge(width = 0.9)
+  pinadmissible <- admissible |>
+    filter(n == n.i, loadings == loadings.i) |>
+    ggplot(aes(x = ncat, y = pinadmissible, colour = method, fill = method)) +
+    geom_col(alpha = 0.2, position = dodge) +
+    facet_grid(rows = vars(skew), scales = "fixed") +
+    coord_cartesian(ylim = c(0, 1)) +
+    scale_y_continuous(labels = scales::label_percent(accuracy = 1)) +
+    ggtitle(sprintf("Percentage inadmissible solutions (n=%i) (lambda=%.1f)", n.i, loadings.i)) +
+    ylab("Percentage inadmissible solutions") +
+    xlab("Categories") +
+    theme_bw()
+
+  # ----------------------------------------------------------------------------
+  # Bias Plots
+  # ----------------------------------------------------------------------------
+
+  dodge <- position_dodge(width = 0.9)
+  plot_bias <- function(param = "Y~X:Z", ci.width = qnorm(0.975)) {
+  
+    filter(df,
+      admissible & par == param[[1]] & n == n.i & loadings == loadings.i
+    ) |>
+    group_by(
+      method, ncat, skew, par
+    ) |>
+    summarize(
+        bias       = mean(bias, na.rm = TRUE),
+        se         = sd(est, na.rm = TRUE),
+        bias.lower = bias - ci.width * se,
+        bias.upper = bias + ci.width * se
+    ) |> print(n=100) |>
+    mutate(
+      ncat = as.factor(ncat),
+      par  = sapply(par, \(p) par2tex[[p]])
+    ) |>
+    ggplot(aes(
+      x = ncat,
+      y = bias,
+      colour = method,
+      ymin = bias.lower,
+      ymax = bias.upper,
+      fill = method
+    )) +
+    geom_col(alpha = 0.2, position = dodge) +
+    geom_errorbar(position = dodge, width = 0.25) +
+    facet_grid(
+      rows = vars(skew),
+      cols = vars(par),
+      scales = "fixed",
+      labeller = label_parsed
+    ) +
+    ggtitle(sprintf("n = %i, loadings = %.1f", n.i, loadings.i)) +
+    ylab("Bias") +
+    xlab("Categories") +
+    theme_bw()
+  }
+
+  # ----------------------------------------------------------------------------
+  # Save
+  # ----------------------------------------------------------------------------
+  
+  plots_bias_b3[[i]] <- plot_bias("Y~X:Z")
+  plots_inadmissible[[i]] <- pinadmissible
+}
+
+target.n <- 1000
+target.l <- 0.8
+idx <- which(simsplit$n == target.n & simsplit$loadings == target.l)
+print(plots_inadmissible[[idx]])
+print(plots_bias_b3[[idx]])
+
